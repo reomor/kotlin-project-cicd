4932f8dec72944ea42f527fbdeeb4902fa5c47b1
Reomor <reomor@gmail.com>
2022-12-03 20:53:13 +0300

    SCH-7 Добавление проверки стиля Kotlin

--- sc-hello-world/src/test/kotlin/com/github/reomor/stringconcat/helloworld/CucumberTestContextConfiguration.kt
+++ sc-hello-world/src/test/kotlin/com/github/reomor/stringconcat/helloworld/CucumberTestContextConfiguration.kt
@@ -4,9 +4,7 @@ import io.cucumber.spring.CucumberContextConfiguration
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 
-//@WebFluxTest
 @SpringBootTest
 @AutoConfigureWebTestClient
 @CucumberContextConfiguration
-open class CucumberTestContextConfiguration {
-}
+open class CucumberTestContextConfiguration
--- sc-hello-world/src/test/kotlin/com/github/reomor/stringconcat/helloworld/RunCucumberTest.kt
+++ sc-hello-world/src/test/kotlin/com/github/reomor/stringconcat/helloworld/RunCucumberTest.kt
@@ -1,15 +1,13 @@
 package com.github.reomor.stringconcat.helloworld
 
+import io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME
 import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.IncludeEngines
 import org.junit.platform.suite.api.SelectClasspathResource
 import org.junit.platform.suite.api.Suite
 
-import io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME
-
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.github.reomor.stringconcat.helloworld")
-open class RunCucumberTest {
-}
+open class RunCucumberTest
